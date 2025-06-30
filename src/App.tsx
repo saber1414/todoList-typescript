0d43f00da0203bfba91a129c9a508d854e7c2c47
Saber___Dev <saber.esmaili1414@gmail.com>
2025-06-30 23:59:42 +0330

    update todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,88 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Todo } from "./components/Todo.type";
+import Swal from "sweetalert2";
 
 import "./App.css";
 
-function App() {
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>(() => {
+    const saveTodo = localStorage.getItem("todos");
+    return saveTodo ? JSON.parse(saveTodo) : [];
+  });
+  const [input, setInput] = useState<string>("");
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  // add todos
+  const addTodo = (e: React.FocusEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    if (!input.trim()) return;
+    const newTodo: Todo = {
+      id: Date.now(),
+      title: input.trim(),
+      isCompleted: false,
+    };
+
+    setTodos((prev: Todo[]) => [...prev, newTodo]);
+    setInput("");
+  };
+
+  // remove todo
+  const removeTodo = (id: number): void => {
+    Swal.fire({
+      icon: "warning",
+      title: "DELETE TODO",
+      text: "Do you want to delete this todo?",
+      showCancelButton: true,
+      confirmButtonText: "YES",
+      cancelButtonText: "NO",
+    }).then((res) => {
+      if (res.isConfirmed) {
+        setTodos((prev: Todo[]) => prev.filter((todo) => todo.id !== id));
+      }
+    });
+  };
+
   return (
-    <div className="TodoWrapper">
-      <h1>Todo List ❤️ </h1>
-      {/* Add New Todo Form */}
-
-      <form className="TodoForm">
-        <input
-          type="text"
-          className="todo-input"
-          placeholder="What is the task today?"
-        />
-        <button type="submit" className="todo-btn">
-          Add Task
-        </button>
-      </form>
-
-      {/* display todos */}
-      <div className="Todo">
-        <p
-          className="" // or completed className
-        >
-          Test Todo Title :))
-        </p>
-        <div>{/* <FontAwesomeIcon icon={faTrash} /> */}</div>
+    <>
+      <div className="TodoWrapper">
+        <h1>Todo List ❤️ </h1>
+        <form onSubmit={addTodo} className="TodoForm">
+          <input
+            type="text"
+            className="todo-input"
+            placeholder="What is the task today?"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <button type="submit" className="todo-btn">
+            Add Task
+          </button>
+        </form>
+        {todos.length ? (
+          todos.map((todo) => (
+            <div key={todo.id} className="Todo">
+              <p
+                className="" // or completed className
+              >
+                {todo.title}
+              </p>
+              <div
+                onClick={() => removeTodo(todo.id)}
+                className="btn btn-danger"
+              >
+                DELETE
+              </div>
+            </div>
+          ))
+        ) : (
+          <p></p>
+        )}
       </div>
-    </div>
+    </>
   );
-}
+};
 
 export default App;
